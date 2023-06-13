5ddc16186ef33f3eb09a85c91c9a24319f9dc9df
Cahya Dinar Prastyo <cahyadinar241@gmail.com>
2023-06-13 23:24:06 +0700

    Create approval feature

--- app/Http/Controllers/ApprovalCutiController.php
+++ app/Http/Controllers/ApprovalCutiController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\JenisCuti;
+use App\Enums\StatusCuti;
+use App\Models\CatatanCuti;
+use App\Models\PerizinanCuti;
+use App\Service\GlobalService;
 use Illuminate\Http\Request;
 
 class ApprovalCutiController extends Controller
 {
-    //
+    public function index(){
+        $data = PerizinanCuti::with(['user', 'user.jabatan', 'atasanLangsung', 'pejabatBerwenang'])
+            ->where('status_persetujuan_atasan_langsung', StatusCuti::PROSES)
+            ->where('status_keputusan_pejabat_berwenang', null)
+            ->get();
+
+        return $this->view('atasan-approval.index', [
+            'data' => $data,
+            'optionsJenisCuti' => JenisCuti::OPTIONS,
+            'statusCutiOptions' => StatusCuti::OPTIONS
+        ], sidebar_menu: 'approval', sidebar_submenu: 'approval.atasan');
+    }
+
+    public function showApproval($id){
+        $data = PerizinanCuti::with(['user', 'user.jabatan', 'atasanLangsung', 'pejabatBerwenang'])
+            ->where('id', $id)
+            ->first();
+
+        $catatanCuti = GlobalService::getCutiHistories($data->user->id);
+
+        return $this->view('atasan-approval.detail', [
+            'data' => $data,
+            'catatanCuti' => $catatanCuti,
+            'optionsJenisCuti' => JenisCuti::OPTIONS,
+            'statusCutiOptions' => StatusCuti::OPTIONS
+        ], sidebar_menu: 'approval', sidebar_submenu: 'approval.atasan');
+    }
+
+    public function approveCuti(Request $request){
+        $status = $request->status;
+        $alasan_pertimbangan = $request->alasan_pertimbangan;
+
+        $perizinan = PerizinanCuti::find($request->_id);
+
+        $perizinan->status_persetujuan_atasan_langsung = $status;
+        $perizinan->alasan_persetujuan_atasan_langsung = $alasan_pertimbangan;
+
+        if($status == StatusCuti::DISETUJUI){
+            $perizinan->status_keputusan_pejabat_berwenang = StatusCuti::PROSES;
+        }
+
+        $perizinan->save();
+
+        return redirect()->back()->with('success', 'Berhasil memberikan pertimbangan cuti pegawai!');
+    }
 }
---
+++ resources/views/atasan-approval/detail.blade.php
@@ -0,0 +1,151 @@
+@extends('template.app')
+@section('title', 'Pengajuan Cuti')
+@section('content')
+  @if($errors->any())
+    @foreach ($errors->all(0) as $error)
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{$error}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    @endforeach
+  @endif
+  <h5>Perizinan Cuti</h5>
+
+  <h6 class="border-bottom mt-2 py-3">Data Pegawai</h6>
+  @include('component.table_pegawai')
+
+  <div class="mt-2 py-2">
+    <h6 class="py-3 border-bottom">Catatan Cuti</h6>
+    <div class="row pt-3">
+      <div class="col-md-2">
+        Cuti Tahunan
+      </div>
+      <div class="col-md-3">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Tahun</th>
+              <th>Sisa</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($catatanCuti as $item)
+                <tr>
+                  <td>{{$item->tahun}}</td>
+                  <td>{{$item->sisa_cuti_tahunan}}</td>
+                </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2">Cuti Sakit</div>
+      <div class="col-md-3">{{$catatanCuti[0]->jumlah_cuti_sakit}}</div>
+    </div>
+    <div class="row">
+      <div class="col-md-2">Cuti Besar</div>
+      <div class="col-md-3">{{$catatanCuti[0]->jumlah_cuti_besar}}</div>
+    </div>
+    <div class="row">
+      <div class="col-md-2">Cuti Alasan Penting</div>
+      <div class="col-md-3">{{$catatanCuti[0]->jumlah_alasan_penting}}</div>
+    </div>
+  </div>
+
+  <div class="mt-2 py-2">
+    <h6 class="py-3 border-bottom">Form Perizinan</h6>
+    <form class="pt-4" method="POST" action="{{route('atasan.approval.approve')}}">
+      <div class="form-group row">
+        <label for="jenis_cuti" class="col-md-2 col-form-label">Jenis Cuti</label>
+        <div class="col-md-8">
+          : {{jenisCuti($data->jenis_cuti_id)}}
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="alasan" class="col-md-2 col-form-label">Alasan Cuti</label>
+        <div class="col-md-8">
+          : {{$data->alasan_cuti}}
+        </div>
+      </div>
+      
+      <div class="form-group row">
+        <label for="bukti" class="col-md-2 col-form-label">Bukti</label>
+        <div class="col-md-8">
+          @if($data->bukti)
+          : <a href="" class="btn btn-outline-primary">Lihat Bukti</a>
+          @else
+          : Tidak ada Bukti
+          @endif
+        </div>
+      </div>
+      
+      <div class="form-group row">
+        <label class="col-md-2 col-form-label">Tanggal</label>
+        <div class="col-md-8">
+            : {{ $data->mulai_cuti }}
+            S/D
+            {{ $data->akhir_cuti }}
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="lama" class="col-md-2 col-form-label">Jumlah Hari</label>
+        <div class="col-md-3">
+          : {{ $data->jumlah_hari }} Hari
+        </div>
+      </div>
+  
+      <div class="form-group row">
+        <label for="alamat" class="col-md-2 col-form-label">Alamat Selama Cuti</label>
+        <div class="col-md-8">
+          : {{ $data->alamat_menjalankan_cuti }}
+        </div>
+      </div>
+      
+      <div class="form-group row">
+        <label for="no_telp" class="col-md-2 col-form-label">No Telpon</label>
+        <div class="col-md-8">
+          : {{ $data->no_telp }}
+        </div>
+      </div>
+
+      <div class="form-group row border-top pt-2">
+        <label for="alasan_pertimbangan" class="col-md-2 col-form-label">Alasan Pertimbangan</label>
+        <div class="col-md-8">
+          @foreach($statusCutiOptions as $key => $option)
+            @if($key != 99)
+              <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="status" id="status-{{$key}}" value="{{$key}}">
+                <label class="form-check-label" for="status-{{$key}}"> {{$option}} </label>
+              </div>
+            @endif
+          @endforeach
+        </div>
+      </div>
+      
+      <div class="form-group row">
+        <label for="alasan_pertimbangan" class="col-md-2 col-form-label">Alasan Pertimbangan</label>
+        <div class="col-md-8">
+          <textarea class="form-control" id="alasan_pertimbangan" rows="3" name="alasan_pertimbangan" required></textarea>
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-md-10 text-right">
+          @csrf
+          <input type="hidden" name="_id" value="{{$data->id}}">
+          <button type="submit" class="btn btn-primary">
+            Submit
+            <i class="fas fa-paper-plane"></i>
+          </button>
+        </div>
+      </div>
+
+    </form>
+  </div>
+  
+@endsection
